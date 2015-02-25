72d6c7d194e33ebf3848e444286bf10d56b3b1f0
Eshin Kunishima <omochi@luna.miko.im>
2015-02-25 20:36:00 +0900

    Wipe out unsed code

--- ext/src/inject.js
+++ ext/src/inject.js
@@ -1,40 +1,45 @@
-chrome.extension.sendMessage({}, function (response) {
-    var readyStateCheckInterval = setInterval(function () {
-        if (document.readyState === 'complete') {
-            clearInterval(readyStateCheckInterval);
+var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === 'complete') {
+        clearInterval(readyStateCheckInterval);
+        ylp();
+    }
+}, 3);
 
-            if ($('.page_title').text() != 'バトル') {
-                return;
-            }
+function ylp() {
+    console.log("ylp loaded");
 
-            $('.data > a').each(function () {
-                var link = $(this).attr('href');
-                var status = $(this).parent();
+    switch ($('.page_title').text()) {
+        case 'バトル':
+            battleShowStatus();
+            break;
+    }
+}
 
-                $.ajax({
-                    url: link,
-                    dataType: "html",
-                    cache: false,
-                    success: function (data, textStatus) {
-                        var atk = 0, def = 0;
+function battleShowStatus() {
+    $('.data > a').each(function () {
+        var link = $(this).attr('href');
+        var status = $(this).parent();
 
-                        $('.deck_thumbnails > li', data).each(function () {
-                            var raws = $(this).text().match(/攻:(\d+)防:(\d+)/);
-                            atk += parseInt(raws[1]);
-                            def += parseInt(raws[2]);
-                        });
+        $.ajax({
+            url: link,
+            dataType: "html",
+            cache: false,
+            success: function (data, textStatus, jqXHR) {
+                var atk = 0, def = 0;
 
-                        console.log($('.page_title', data).text() + ' → ATK : ' + atk + " / DEF : " + def);
-
-                        status.append('A: ' + atk + " D: " + def);
-                    },
-                    error: function (xhr, textStatus, errorThrown) {
-                        console.log('Error: ' + link);
-                    }
+                $('.deck_thumbnails > li', data).each(function () {
+                    var raws = $(this).text().match(/攻:(\d+)防:(\d+)/);
+                    atk += parseInt(raws[1]);
+                    def += parseInt(raws[2]);
                 });
-            });
 
-            console.log("I love lol cats.");
-        }
-    }, 1);
-});
+                console.log($('.page_title', data).text() + ' → ATK : ' + atk + " / DEF : " + def);
+
+                status.append('A: ' + atk + " D: " + def);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('Error: ' + errorThrown + ' / ' + link);
+            }
+        });
+    });
+}
